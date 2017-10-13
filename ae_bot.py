777b7332943cbbea71510c78d7243ef3f8214076
Xilin Sun <s.sn.giraffe@gmail.com>
2017-10-13 00:36:39 -0700

    update bot program

---
+++ ae_bot.py
@@ -0,0 +1,30 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import discord
+from discord.ext import commands
+import asyncio
+import json
+
+
+# In[2]:
+
+
+# import logging# logger = logging.getLo('aeb')
+# hdlr = logging.FileHandler('aeb.log')
+# formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+# hdlr.setFormatter(formatter)
+# if len(logger.handlers) == 0:
+#     logger.addHandler(hdlr)
+# print len(logger.handlers):
+#     logger.setLevel(logging.DEBUG)
+
+
+# In[ ]:
+
+
+
+
